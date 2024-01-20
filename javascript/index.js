fd62ecfcf40942faa944147bbbe2f08e8951ece3
demimortimer <153766264+demimortimer@users.noreply.github.com>
2024-01-20 13:11:24 +0000

    lesson 6

--- javascript/index.js
+++ javascript/index.js
@@ -21,5 +21,27 @@ function updateParisTime() {
   parisDateElement.innerHTML = parisTimezone.format("MMMM Do YYYY");
   parisTimeElement.innerHTML = `${parisTime} <small>${parisAmPm}</small>`;
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 setInterval(updateLosAngelesTime, 1000);
 setInterval(updateParisTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
